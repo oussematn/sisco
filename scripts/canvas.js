5e04dcbecf250f1ade999cd085570dfc1674f14f
OussemaMiled <56867525+OussemaMiled@users.noreply.github.com>
2019-11-29 17:00:15 +0100

    some changes

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,7 +1,24 @@
+class AP {
+  constructor(absc, ord, radius) {
+    this.absc = absc;
+    this.ord = ord;
+    this.radius = radius;
+  }
+}
+
+class obstacle {
+  constructor(absc, ord, owidth, oheigth) {
+    this.absc = absc;
+    this.ord = ord;
+    this.owidth = owidth;
+    this.oheight = oheigth;
+  }
+}
+
 // Choosing material
 let material;
 let materials = document.querySelectorAll("img"); // Return a nodeList (can apply array method)
-
+let disableAddObstacle = 0;
 
 for (var i in materials) {
   materials[i].onclick = function () {
@@ -15,12 +32,19 @@ function selectMaterial(e) {
   console.log(material);
 }
 
-// Drawing
+//! Drawing
+let obstacles = [];
 initDraw(document.getElementById('canvas'));
 
 function initDraw(canvas) {
   function setMousePosition(e) {
     var ev = e || window.event;
+    /*  --pageX pageY :Output the coordinates (according to the document) of the mouse pointer when the mouse button is clicked on an element
+          --The clientX read-only property of the MouseEvent interface provides the horizontal coordinate within the application's client area
+           at which the event occurred (as opposed to the coordinate within the page).
+          For example, clicking on the left edge of the client area will always result in a mouse event with a clientX value of 0,
+          regardless of whether the page is scrolled horizontally. 
+    */
     if (ev.pageX) {
       mouse.x = ev.pageX + window.pageXOffset;
       mouse.y = ev.pageY + window.pageYOffset;
@@ -37,12 +61,15 @@ function initDraw(canvas) {
     startY: 0
   };
   var element = null;
-
+  let width;
+  let height;
   canvas.onmousemove = function (e) {
     setMousePosition(e);
-    if (element !== null) {
+    if ((element !== null) && (!disableAddObstacle)) {
       element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+      width = element.style.width;
       element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+      height = element.style.height;
       element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x - 250 + 'px' : mouse.startX - 250 + 'px';
       element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
     }
@@ -61,11 +88,14 @@ function initDraw(canvas) {
       element.style.top = mouse.y + 'px';
       canvas.appendChild(element)
       canvas.style.cursor = "crosshair";
+      obstacles.push(new obstacle(mouse.x, mouse.y, width, height));
+      console.log(obstacles);
     }
   }
+
 }
 
-// Add access point
+//! Add access point
 const addBtn = document.querySelector('#addAccessBtn');
 let accessPoints = [];
 addBtn.addEventListener('click', addAccessPoint);
@@ -76,31 +106,41 @@ function addAccessPoint() {
   canvas.addEventListener('mouseenter', (e) => {
     canvas.style.cursor = "url('/images/ap.png'), auto";
   });
+  let c = document.getElementById("innerCanvas");
+  let ctx = c.getContext("2d");
+  // Make inner canvas full width and heigth
+  c.width = document.body.clientWidth;
+  c.height = document.body.clientHeight;
+  canvasW = c.width;
+  canvasH = c.height;
   canvas.addEventListener('click', (e) => {
     radius = Number(radius);
-    let c = document.getElementById("innerCanvas");
-    let ctx = c.getContext("2d");
-    // Make inner canvas full width and heigth
-    c.width = document.body.clientWidth;
-    c.height = document.body.clientHeight;
-    canvasW = c.width;
-    canvasH = c.height;
     ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI);
+    ctx.arc(e.clientX - 250, e.clientY, radius, 0, 2 * Math.PI);
     ctx.stroke();
     //calculating colors
-    let green = (radius / 100) * 30;
     let x = e.clientX;
     let y = e.clientY;
     // Radii of the white glow.
-    let innerRadius = green;
-    let outerRadius = green + ((green / 100) * 20);
-    let gradient = ctx.createRadialGradient(x, y, innerRadius, x-250, y, outerRadius);
-    gradient.addColorStop(0, '#04ff00');
-    gradient.addColorStop(1, 'red');
-    ctx.arc(x-250, y, radius, 0, 2 * Math.PI);
+    let innerRadius = radius * 0.04;
+    let outerRadius = radius * 1.25;
+    let gradient = ctx.createRadialGradient(x - 250, y, innerRadius, x - 250, y, outerRadius);
+    gradient.addColorStop(0, 'rgba(4, 255, 0,0.7)');
+    gradient.addColorStop(1, 'rgba(0, 191, 255,0.6)');
+    ctx.arc(x - 250, y, radius, 0, 2 * Math.PI);
     ctx.fillStyle = gradient;
     ctx.fill();
+    disableAddObstacle = 1;
+    accessPoints.push(new AP(e.clientX, e.clientY, radius));
+
+    // create text on a circle
+    let txt = document.createElement('div');
+    txt.innerHTML = `AP${accessPoints.length}`;
+    txt.style.position = 'absolute';
+    txt.style.left = `${e.clientX - 250}px`;
+    txt.style.top = `${e.clientY}px`;
+    txt.style.zIndex = "100000";
+    canvas.appendChild(txt);
   });
 }
 
@@ -110,3 +150,17 @@ document.querySelector("#sidebar").addEventListener('mouseenter', () => {
     canvas.style.cursor = "default";
   });
 });
+
+// Selecting access point
+
+// Event listener on the button
+let resultBtn = document.querySelector('#selectAccessBtn');
+resultBtn.addEventListener('click', selectAP);
+function selectAP() {
+  // Shift array with 1 position because of a latency in the onclick event
+  for (let i = 0; i < obstacles.length-1 ;i++) {
+    obstacles[i].owidth=obstacles[i+1].owidth;
+    obstacles[i].oheight=obstacles[i+1].oheight;
+  }
+  obstacles.splice(-1,1);
+}
